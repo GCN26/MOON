959eb65b1569bbd4bebeab3b640b20bd947b3b83
GSheldon262 <gsheldon@ltu.edu>
2024-11-21 22:42:58 -0500

    Retro Movement Toggle
    There are now two different modes of camera movement: Retro and Modern
    Retro uses A and D / Left and Right to move the camera. The player cannot sidestep in this mode.
    Modern uses the mouse to move the camera left and right. The player has the ability to sidestep in this mode.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,13 +9,15 @@ public class PlayerScript : MonoBehaviour
     public float speed = 5.0f;
 
     //Camera variables
-    public bool cursorFree;
+    private float rotX,rotY,xVelocity,yVelocity;
+    public float mouseSensitivity, snappiness, upDownRange;
+    public bool cursorFree,retroMovement;
     private Vector3 direction;
 
     public CharacterController control;
     public GameObject mainCam;
 
-    public Vector3 rotation;
+    public Vector3 rotation,move;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,12 +29,29 @@ public class PlayerScript : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        //consider adding toggle for boomer vs modern camera settings
+        //thats if i can get modern to work
+        rotX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        rotY -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        xVelocity = Mathf.Lerp(xVelocity, rotX, snappiness * Time.deltaTime);
+        yVelocity = Mathf.Lerp(yVelocity, rotY, snappiness * Time.deltaTime);
+
+        if (!retroMovement)
+        {
+            transform.Rotate(0, xVelocity, 0);
+            move = new Vector3(horizontal*Time.deltaTime, 0, vertical * Time.deltaTime);
+        }
+        if (retroMovement)
+        {
+            this.rotation = new Vector3(0, horizontal * 180 * Time.deltaTime, 0);
+            move = new Vector3(0, 0, vertical * Time.deltaTime);
+        }
+
         direction = new Vector3(horizontal, 0, vertical);
         if(!cursorFree) Cursor.lockState = CursorLockMode.Locked;
 
-        this.rotation = new Vector3(0, horizontal * 180 * Time.deltaTime, 0);
-
-        Vector3 move = new Vector3(0, 0, vertical * Time.deltaTime);
+        
         move = this.transform.TransformDirection(move);
         control.Move(move * speed);
         this.transform.Rotate(this.rotation);
